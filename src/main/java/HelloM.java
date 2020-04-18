89480d65c6730c614c2e7cd9430357ab6c930e2d
zhangweisen <zws812000@sina.cn>
2020-04-18 15:21:07 +0800

    feat: 1. 新增HelloM in branch master
    Signed-off-by: zhangweisen <zws812000@sina.cn>

---
+++ src/main/java/HelloM.java
@@ -0,0 +1,14 @@
+/**
+ * @author Vincent
+ * @since 2020/4/18
+ */
+public class HelloM {
+
+
+
+
+
+    public static void main(String[] args) {
+        System.out.println("hello master");
+    }
+}
